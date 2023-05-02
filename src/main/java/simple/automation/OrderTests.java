aac2ba5db2a9e639440370eff13b9beaccd3e0d0
Musalini <muradzade.musa@gmail.com>
2023-05-02 21:11:02 +0300

    add maven

--- src/main/java/simple/automation/OrderTests.java
+++ src/main/java/simple/automation/OrderTests.java
@@ -1,10 +1,13 @@
 package simple.automation;
 
+import org.junit.Test;
+
 public class OrderTests extends BaseTestWithOrder {
     void testCreateOrder() {
         System.out.println("Check order creating");
     }
 
+    @Test
     void testListOrders() {
         System.out.println("list orders");
         System.out.println("check order displaying");
